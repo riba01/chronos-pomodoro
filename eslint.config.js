7bb842b978a1deda4b4703f5e7e771040517972b
Armando Ribamar <armando.ribamar@gmail.com>
2025-06-15 13:30:38 -0300

    New eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -23,6 +23,12 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // 🔥 Style rules
+      semi: ['error', 'always'], // Obrigatório ponto e vírgula
+      quotes: ['error', 'single'], // Aspas simples
+      'comma-dangle': ['error', 'always-multiline'], // Vírgula no final de objetos/arrays multilinha
+      'object-curly-spacing': ['error', 'always'], // Espaço dentro de objetos { exemplo: true }
+      'arrow-parens': ['error', 'as-needed'], // Remove parênteses em arrow functions com 1 parâmetro
     },
   },
-)
+);
