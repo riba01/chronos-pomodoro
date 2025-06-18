7a8267a9a0dcfc33fa7a871e68fdec0d6869694e
Armando Ribamar <armando.ribamar@gmail.com>
2025-06-18 13:53:47 -0300

    aula 50

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,11 +2,17 @@ import { CircleChevronRightIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    /* console.log('Deu certo'); */
+  }
   return (
     <>
-      <form action="" className='form'>
+      <form onSubmit={handleCreateNewTask} action="" className='form'>
         <div className="formRow">
           <DefaultInput
             type='text'
@@ -14,6 +20,8 @@ export function MainForm() {
             labelText='TASK'
             title='Inserir as Tasks'
             placeholder='Digite a tarefa'
+            onChange={(e) => setTaskName(e.target.value)}
+            value={taskName}
           />
         </div>
         <div className='formRow'>
